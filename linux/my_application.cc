1cbe631a65d212ea367fd2361d57c9d7e398dda9
Souvik Hazra <souvik.babanhazra@gmail.com>
2022-09-14 23:58:32 +0530

    Window title and icon changed for linux

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,14 +40,15 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "vector_icon_generator");
+    gtk_header_bar_set_title(header_bar, "Vector Icon Generator");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "vector_icon_generator");
+    gtk_window_set_title(window, "Vector Icon Generator");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
+  gtk_window_set_icon_name(window, "vector-icon-generator");
   gtk_widget_show(GTK_WIDGET(window));
 
   g_autoptr(FlDartProject) project = fl_dart_project_new();
